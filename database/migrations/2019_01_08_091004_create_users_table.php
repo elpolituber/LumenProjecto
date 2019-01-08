2c4e41fae6485afd6fc8938af561132553dba2b1
elpolituber <franco-roko@outlook.com>
2019-01-08 09:36:29 -0500

    segundo cambio

---
+++ database/migrations/2019_01_08_091004_create_users_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id_user');
+            $table->string('nombre');
+            $table->string('apellido');
+	        $table->string('foto');	
+            $table->string('usuario');
+            $table->string('email')->unique();
+            $table->string('pasword');
+            $table->timestamps();
+        });
+
+        Schema::create('publications', function (Blueprint $table) {
+            $table->increments('id_publication');
+            $table->string('detalle');           
+	        //primary key foreing key user
+	    });
+        
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id_publication');
+            $table->string('detalle');           
+	        //primary key foreing key user	
+	    });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('publications');
+        Schema::dropIfExists('chats');
+    
+    }
+}
